aef256baa2e5b78e7bafd9ac2b3fc9bb86ea4d06
Fareed <fareed.ch7869@gmail.com>
2024-09-10 20:03:22 +0500

    feat(navbar): add navbar and set up project

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="text-white min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
+      <h1 className="text-2xl text-center">Welcome to Music Academy Project</h1>
+    </main>
+  );
+}
